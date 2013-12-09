46eca9ce5bf310b93720607fbe1037d5ea8b8f27
Rubén Gil <rubengil22@gmail.com>
2013-12-09 17:38:08 +0100

    Created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shareup::Application.config.secret_key_base = 'e31201a9bab782951fb0b197a9a6b883a5bf9ac30ad5b973710f99bcd1fb5e0aea29b66ec5c617b224220919f6fa76641dd499a8ac50bf9e4ce922cb7a564742'
