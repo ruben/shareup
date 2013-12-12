c795f9bbe1a045e06122c694b52d301cfbfceac1
Rubén Gil <rubengil22@gmail.com>
2013-12-12 14:46:12 +0100

    Fixed devise initializer

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -2,4 +2,4 @@ require 'ostruct'
 require 'yaml'
 
 config = YAML.load_file(File.join(Rails.root, 'config', 'app_config.yml')) || {}
-app_config = OpenStruct.new(config[Rails.env] || {})
+AppConfig = OpenStruct.new(config[Rails.env] || {})
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -228,7 +228,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :twitter, app_config.twitter['client_id'], app_config.twitter['client_secret']
+  config.omniauth :twitter, AppConfig.twitter['client_id'], AppConfig.twitter['client_secret']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
